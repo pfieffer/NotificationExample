70f1143d83434744be18fa5b50b31a03059ca265
RaviGarbujaPun <cbpmgr@gmail.com>
2017-06-11 17:02:44 +0545

    Notification done

--- app/src/main/java/com/example/ravi/notificationexample/MainActivity.java
+++ app/src/main/java/com/example/ravi/notificationexample/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.ravi.notificationexample;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnClickMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnClickMe = (Button) findViewById(R.id.button_click);
+
+        btnClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickedFunc();
+            }
+        });
+    }
+
+    private void clickedFunc() {
+        //build Notification
+        NotificationCompat.Builder mBuilder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Simple Notification")
+                .setContentText("This is a test of simple notification.");
+
+        //get an instance of the NotificationManager service
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //post notification to the notification bar
+        notificationManager.notify(0,mBuilder.build());
+
     }
 }
