7f95e7901e04bc9689be029fec4209fa0ad954af
RaviGarbujaPun <cbpmgr@gmail.com>
2017-06-11 17:09:18 +0545

    Rename button id

--- app/src/main/java/com/example/ravi/notificationexample/MainActivity.java
+++ app/src/main/java/com/example/ravi/notificationexample/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnClickMe = (Button) findViewById(R.id.button_click);
+        btnClickMe = (Button) findViewById(R.id.button_simple);
 
         btnClickMe.setOnClickListener(new View.OnClickListener() {
             @Override
